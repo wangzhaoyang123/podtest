4d473438870907dc46ab1b38a9c85256aa0b17a5
wangzhaoyang <wangzhaoyang@lexue.com>
2022-08-02 19:45:44 +0800

    yy

---
+++ Framework/Podtest.framework/Headers/Podtest.h
@@ -0,0 +1,20 @@
+//
+//  Podtest.h
+//  Podtest
+//
+//  Created by wangzhaoyang on 2022/8/2.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Podtest.
+FOUNDATION_EXPORT double PodtestVersionNumber;
+
+//! Project version string for Podtest.
+FOUNDATION_EXPORT const unsigned char PodtestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Podtest/PublicHeader.h>
+import <Podtest/Podadd.h>
+
+
+
